e39ab3febbd305a405c7dca9cbca93445a2dd1a4
Matthew Lewis <matt@mplewis.com>
2016-07-01 17:28:23 -0600

    vc: big improvements to polls controller, views

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,9 +1,11 @@
 class PollsController < ApplicationController
   before_action :set_poll, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_user_logged_in, only: [:index, :create, :update, :destroy]
+  before_action :ensure_owner!, only: [:update, :destroy]
 
   # GET /polls
   def index
-    @polls = Poll.all
+    @polls = Poll.where user: current_user
   end
 
   # GET /polls/1
@@ -21,7 +23,7 @@ class PollsController < ApplicationController
 
   # POST /polls
   def create
-    @poll = Poll.new(poll_params)
+    @poll = current_user.polls.build(poll_params)
 
     if @poll.save
       redirect_to @poll, notice: 'Poll was successfully created.'
@@ -32,6 +34,7 @@ class PollsController < ApplicationController
 
   # PATCH/PUT /polls/1
   def update
+    ensure_owner!
     if @poll.update(poll_params)
       redirect_to @poll, notice: 'Poll was successfully updated.'
     else
@@ -41,18 +44,24 @@ class PollsController < ApplicationController
 
   # DELETE /polls/1
   def destroy
+    ensure_owner!
     @poll.destroy
     redirect_to polls_url, notice: 'Poll was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_poll
-      @poll = Poll.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_poll
+    @poll = Poll.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def poll_params
-      params.require(:poll).permit(:user_id, :text)
-    end
+  # Only allow a trusted parameter "white list" through.
+  def poll_params
+    params.require(:poll).permit(:text)
+  end
+
+  # Only proceed with this action if the poll belongs to the current user.
+  def ensure_owner!
+    redirect_to @poll, alert: 'This poll can only be modified by its creator.' unless current_user == @poll.user
+  end
 end
