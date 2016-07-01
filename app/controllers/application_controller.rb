740f47d9353984b2bc697e202eb33f5c9b820360
Matthew Lewis <matt@mplewis.com>
2016-07-01 17:27:09 -0600

    app: devise auth isnt working; write the helper manually

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+  def ensure_user_logged_in
+    redirect_to new_user_session_path, notice: 'You must be logged in to do that.' unless user_signed_in?
+  end
 end
